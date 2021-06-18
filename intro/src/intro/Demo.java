4b3c8fd8d400da30fa6c3c4cd5cdf2145779c654
vagi07 <vagi.buddy@gmail.com>
2021-06-18 15:42:31 +0530

    First commit

---
+++ intro/src/intro/Demo.java
@@ -0,0 +1,26 @@
+package intro;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+	System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");	
+	
+	WebDriver driver=new ChromeDriver();
+		
+	driver.get("https://www.facebook.com/");
+	
+	driver.findElement(By.id("email")).sendKeys("m.vageeshjajur@gmail.com");
+	driver.findElement(By.id("pass")).sendKeys("9620602353");
+	
+	
+	driver.close();
+		
+	}
+
+}
