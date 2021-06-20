c437b289526a19da6f7ff5efa347f5fc9fbadb6c
vagi07 <vagi.buddy@gmail.com>
2021-06-20 10:33:03 +0530

    reverse string

---
+++ intro/src/intro/reversestring.java
@@ -0,0 +1,23 @@
+package intro;
+
+public class reversestring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String life = "lifeisconfusingenoughtogetdistracted";
+		 
+        // getBytes() method to convert string
+        // into bytes[].
+        byte[] strAsByteArray = life.getBytes();
+ 
+        byte[] result = new byte[strAsByteArray.length];
+ 
+        // Store result in reverse order into the
+        // result byte[]
+        for (int i = 0; i < strAsByteArray.length; i++)
+            result[i] = strAsByteArray[strAsByteArray.length - i - 1];
+ 
+        System.out.println(new String(result));
+	}
+
+}
