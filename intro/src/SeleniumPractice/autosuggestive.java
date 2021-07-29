c8c45f5d759d9b2f6aaed45b5fc38d3ba4001011
vagi07 <vagi.buddy@gmail.com>
2021-07-29 17:15:12 +0530

    Ready

---
+++ intro/src/SeleniumPractice/autosuggestive.java
@@ -0,0 +1,33 @@
+package intro;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class autosuggestive {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+
+		driver.findElement(By.id("autosuggest")).sendKeys("ind");
+		Thread.sleep(3000);
+		List<WebElement> options = driver.findElements(By.cssSelector("li[class='ui-menu-item'] a"));
+
+		for (WebElement option : options) {
+			if (option.getText().equalsIgnoreCase("India")) {
+				option.click();
+				break;
+			}
+		}
+
+	}
+
+}
