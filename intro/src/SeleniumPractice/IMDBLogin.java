547443a0b3ca11f31ba61bcb877aefeed02db230
Vageesha Jajur <m.vageeshjajur@gmail.com>
2021-08-01 09:27:07 +0530

    reading fileproperteis

--- intro/src/SeleniumPractice/IMDBLogin.java
+++ intro/src/SeleniumPractice/IMDBLogin.java
@@ -1,41 +1,79 @@
 package SeleniumPractice;
 
-import java.util.concurrent.TimeUnit;
-
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
 
 public class IMDBLogin {
-	static WebDriver driver;
-	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");	
 
-		driver=new ChromeDriver();
-		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		driver.manage().window().maximize();
-		driver.manage().deleteAllCookies();
-		
-		//System.setProperty("webdriver.edge.driver", "C:\\Users\\VAGI\\git\\Selenium-Java-automation\\intro\\driver\\EdgeDriver");
-		//WebDriver driver = new EdgeDriver();
+	public static void main(String[] args) throws IOException, InterruptedException {
+		// Loading properties file
+		WebDriver driver = null;
+		Properties prop = new Properties();
+		FileInputStream ip = new FileInputStream(
+				"C:/Users/VAGI/git/Selenium-Java-automation/intro/src/q/config/IMDBfile.properties");
+		prop.load(ip);
 
-		driver.get("https://www.imdb.com/");
+		// intiating different broswsers
+		String browserName = prop.getProperty("browser");
+
+		if (browserName.equals("chrome")) {
+
+			System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+
+			driver = new ChromeDriver();
+
+		} else if (browserName.equals("FF")) {
+
+			System.setProperty("webdriver.gecko.driver",
+					"C:/Users/VAGI/git/Selenium-Java-automation/intro/driver/geckodriver/geckodriver.exe");
+
+			driver = new FirefoxDriver();
+		}
+
+		else if (browserName.equals("Microsoft Edge")) {
+
+			System.setProperty("webdriver.edge.driver", "./driver/EdgeDriver/msedgedriver.exe");
+			driver = new EdgeDriver();
+		}
+
+		else if (browserName.equals("Opera")) {
+
+			System.setProperty("webdriver.opera.driver",
+					"C:/Users/VAGI/git/Selenium-Java-automation/intro/driver/operadriver/operadriver.exe");
+			driver = new OperaDriver();
+
+		}
+
+		else {
+
+			System.out.println("No browser is given");
+		}
+
+		// Test Scenario
+		String PageTitle = driver.getTitle();
+		System.out.println("Title of the Page is: " + PageTitle);
+
+		driver.get(prop.getProperty("url"));
 		driver.manage().window().maximize();
-		driver.findElement(By.xpath("/html[1]/body[1]/div[2]/nav[1]/div[2]/div[5]/a[1]/div[1]")).click();
-		driver.findElement(By.xpath("//div[@id='wrapper']//a[1]//span[2]")).click();
-		driver.findElement(By.cssSelector("#ap_email")).sendKeys("vagi.buddy@gmail.com");
-		driver.findElement(By.cssSelector("#ap_password")).sendKeys("dLZ+vj#G-BAZ6M@");
-		driver.findElement(By.cssSelector("#signInSubmit")).click();
-		driver.findElement(By.xpath("//body/div[@id='__next']/nav[@id='imdbHeader']/div[2]/div[5]/div[1]/label[2]/div[1]/span[1]")).click();
-		driver.findElement(By.cssSelector(".ipc-list__item[href='/profile/lists?ref_=nv_usr_lst_3']")).click();
-		driver.findElement(By.xpath("//li[@id='ls091879973']//a[@class='list-name']")).click();
-		driver.findElement(By.xpath("//*[name()='path' and contains(@d,'M12 8c1.1 ')]")).click();
-		driver.findElement(By.cssSelector("div[id='wrapper'] li:nth-child(2) a:nth-child(1)")).click();
-		Thread.sleep(6000);
+		driver.findElement(By.xpath(prop.getProperty("sign_in_first"))).click();
+		driver.findElement(By.xpath(prop.getProperty("sign_with_imdb"))).click();
+		driver.findElement(By.id(prop.getProperty("id"))).sendKeys(prop.getProperty("Email"));
+		driver.findElement(By.name(prop.getProperty("name"))).sendKeys(prop.getProperty("Password"));
+		driver.findElement(By.xpath(prop.getProperty("sign_in"))).click();
+		driver.findElement(By.xpath(prop.getProperty("vagi"))).click();
+		driver.findElement(By.cssSelector(prop.getProperty("your_list"))).click();
+		driver.findElement(By.xpath(prop.getProperty("must_watch_tv_shows"))).click();
+		driver.findElement(By.xpath(prop.getProperty("three_dots"))).click();
+		driver.findElement(By.xpath(prop.getProperty("export"))).click();
+		Thread.sleep(5000);
 		driver.close();
-
 
 	}
 
